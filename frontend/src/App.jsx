d140d1341ed9b0d4100af30e55a1578e974461de
Yadnesh <yadneshm246@gmail.com>
2026-09-20 10:44:13 +0530

    Update TrueWeight

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./index.css";
 
-const BACKEND_URL = "http://192.168.29.127:8000";
+const BACKEND_URL = "http://10.23.93.59:8000";
 
 const ERROR_LIMIT_G = 50;
 const ERROR_LIMIT_KG = 0.05;
@@ -1498,7 +1498,7 @@ if (isPublicVerifyPage) {
             <button
               className="secondary-button"
               onClick={() =>
-                 setPage("role-select")
+                setPage("role-select")
               }
             >
               ← Back to Login Selection
